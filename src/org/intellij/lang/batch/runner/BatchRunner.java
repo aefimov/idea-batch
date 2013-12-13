963b18422611dc9b28bbb314aa447e20aa090d26
denis.bogdanas <denis.bogdanas@gmail.com>
2013-12-13 08:38:55 +0000

    Implemented version 1.0.4: 1. Support for IDEA 13. 2. Support for empty run configurations.

--- src/org/intellij/lang/batch/runner/BatchRunner.java
+++ src/org/intellij/lang/batch/runner/BatchRunner.java
@@ -1,8 +1,15 @@
 package org.intellij.lang.batch.runner;
 
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.ExecutionResult;
 import com.intellij.execution.configurations.RunProfile;
-import com.intellij.execution.executors.DefaultRunExecutor;
+import com.intellij.execution.configurations.RunProfileState;
 import com.intellij.execution.runners.DefaultProgramRunner;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.runners.RunContentBuilder;
+import com.intellij.execution.ui.RunContentDescriptor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -15,6 +22,21 @@ public class BatchRunner extends DefaultProgramRunner {
     }
 
     public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile) {
-        return executorId.equals(DefaultRunExecutor.EXECUTOR_ID) && profile instanceof BatchRunConfiguration;
+        return profile instanceof BatchRunConfiguration;
     }
+
+    @Override
+    protected RunContentDescriptor doExecute(final Project project,
+                                             final RunProfileState state,
+                                             final RunContentDescriptor contentToReuse,
+                                             final ExecutionEnvironment env) throws ExecutionException {
+        FileDocumentManager.getInstance().saveAllDocuments();
+
+        ExecutionResult executionResult = state.execute(env.getExecutor(), this);
+        if (executionResult == null || executionResult.getExecutionConsole() == null) return null;
+
+        final RunContentBuilder contentBuilder = new RunContentBuilder(this, executionResult, env);
+        return contentBuilder.showRunContent(contentToReuse);
+    }
+
 }
